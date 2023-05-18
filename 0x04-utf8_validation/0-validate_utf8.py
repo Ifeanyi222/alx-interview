b018740055865ec85bf4bee407f7eb21cdd98ddc
Ifeanyi222 <linusify@ymail.com>
2023-05-18 12:51:33 +0000

    Done

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Write a method that determines if a given data 
+set represents a valid UTF-8 encoding.
+"""
+def validUTF8(data):
+
+    expected_continuation_bytes = 0
+
+    UTF8_BIT_1 = 1 << 7  
+    UTF8_BIT_2 = 1 << 6  
+
+    for byte in data:
+        leading_one_mask = 1 << 7
+
+        if expected_continuation_bytes == 0:
+            while leading_one_mask & byte:
+                expected_continuation_bytes += 1
+                leading_one_mask = leading_one_mask >> 1
+
+            if expected_continuation_bytes == 0:
+                continue
+
+            if expected_continuation_bytes == 1 or\
+                    expected_continuation_bytes > 4:
+                return False
+
+        else:
+            if not (byte & UTF8_BIT_1 and not (byte & UTF8_BIT_2)):
+                return False
+
+        expected_continuation_bytes -= 1
+
+    if expected_continuation_bytes == 0:
+        return True
+    else:
+        return False
