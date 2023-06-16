baf1fb13a4b6a0368cee45471bd4e11d699e3c08
Ifeanyi222 <linusify@ymail.com>
2023-06-16 02:14:13 +0000

    Done

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """ fewest number of coins needed """
+    if total <= 0:
+        return 0
+    # sort 
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
